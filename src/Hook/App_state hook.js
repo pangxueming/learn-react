2de2d79ca9756705d16783aafc638d3d8d0f57cd
pangxueming <pangxueming@82cf.com>
2021-10-26 15:10:36 +0800

    hook中使用context

--- src/Hook/App_state hook.js
+++ src/Hook/App_state hook.js
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+
 
 function App() {
   const [count, setCount] = useState(0);
-  useEffect(() => {
-    document.title = `You click ${count} times`;
-  })
+
+  // 声明多个 state 变量！
+  const [age, setAge] = useState(42);
+  const [fruit, setFruit] = useState('banana');
+  const [todos, setTodos] = useState([{ text: 'Learn Hooks' }]);
+  
   return (
     <div>
       <p>You click {count} times</p>
