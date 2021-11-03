559b25bc903f337b5ff1cdda2fb5d2d75e8f0b66
pangxueming <6551873+pangxueming@user.noreply.gitee.com>
2021-11-03 21:30:57 +0800

    style in js

--- src/react-css/04_styled-components/home/index.js
+++ src/react-css/04_styled-components/home/index.js
@@ -1,14 +1,47 @@
 import React, { PureComponent } from 'react'
+import styled from 'styled-components'
+
+const HomeWrapper = styled.div`
+  font-size:50px;
+  color:red;
+
+  .banner{
+    background-color: blue;
+
+    span{
+      color: #fff;
+
+      &.active{
+        color: red;
+      }
+
+      &:hover{
+        color: green;
+      }
+
+      &::after{
+        content: 'aaa';
+      }
+    }
+  }
+`
+
+const TitleWrapper = styled.h2`
+  text-decoration: underline;
+`
 
 export default class Home extends PureComponent {
   render() {
     return (
-      <div>
-        <h2>我是home组件的标题</h2>
-        <div>
-          <span>轮播图</span>
+      <HomeWrapper>
+        <TitleWrapper>我是home组件的标题</TitleWrapper>
+        <div className="banner">
+          <span>轮播图1</span>
+          <span>轮播图2</span>
+          <span className="active">轮播图3</span>
+          <span>轮播图4</span>
         </div>
-      </div>
+      </HomeWrapper>
     )
   }
 }
--- src/react-css/04_styled-components/profile/index.js
+++ src/react-css/04_styled-components/profile/index.js
@@ -1,9 +1,32 @@
 import React, { PureComponent } from 'react'
+import styled from 'styled-components'
+/**
+ * 1.穿透
+ */
+
+const ProfileInput = styled.input.attrs({
+  placeholder: 'zhangsan',
+  bColor: 'red'
+})`
+  background-color: skyblue;
+  border-color: ${props => props.bColor};
+  color:${props => props.color}
+`
 
 export default class Profile extends PureComponent {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      color: 'purple'
+    }
+  }
+
   render() {
     return (
       <div>
+        <ProfileInput type="text" color={this.state.color} />
         <h2>我是Profile组件的标题</h2>
         <div>
           <span>setting</span>
