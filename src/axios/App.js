f6e5a8ca06a9b337491083e1041459ff3c5248dd
pangxueming <pangxueming@82cf.com>
2021-11-09 18:03:15 +0800

    aync await 用try cath捕获错误

--- src/axios/App.js
+++ src/axios/App.js
@@ -11,7 +11,7 @@ export default class App extends PureComponent {
     }
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     // axios({
     //   url: 'https://httpbin.org/get',
     //   params: {
@@ -37,18 +37,28 @@ export default class App extends PureComponent {
     //   console.log(err);
     // })
 
-    axios.get('https://httpbin.org/get', {
-      params: {
-        name: 'zhangsan',
-        age: 18
-      }
-    }).then(console.log)
+    // axios.get('https://httpbin.org/get', {
+    //   params: {
+    //     name: 'zhangsan',
+    //     age: 18
+    //   }
+    // }).then(console.log)
 
 
-    axios.post('https://httpbin.org/post', {
-      name: 'lisi',
-      age: 19
-    }).then(console.log)
+    // axios.post('https://httpbin.org/post', {
+    //   name: 'lisi',
+    //   age: 19
+    // }).then(console.log)
+
+    try {
+      const result = await axios.post('https://httpbin.org/post', {
+        name: 'lisi',
+        age: 19
+      });
+      console.log(result);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   render() {
